6bf1016f866b4d74ce46dc852f8a9b9cecd0640c
Jan Phillip Kretzschmar <janphkre@gmx.de>
2019-07-29 00:23:04 +0200

    Android: adapted uuid generation

--- android-app/p2p/src/main/java/de/zweidenker/p2p/model/bluetooth/BluetoothDetails.kt
+++ android-app/p2p/src/main/java/de/zweidenker/p2p/model/bluetooth/BluetoothDetails.kt
@@ -16,8 +16,8 @@ data class BluetoothDetails(
     )
 
     constructor(txtRecordMap: Map<String, String>): this(
-        txtRecordMap[P2PModule.KEY_BLUETOOTH_MAC] ?: "",
-        UUID.fromString("${P2PModule.TYPE_SERVICE}${txtRecordMap[P2PModule.KEY_BLUETOOTH_MAC]}")
+        txtRecordMap.getValue(P2PModule.KEY_BLUETOOTH_MAC),
+        generateUuid(txtRecordMap.getValue(P2PModule.KEY_BLUETOOTH_MAC), P2PModule.TYPE_SERVICE)
     )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
@@ -29,7 +29,15 @@ data class BluetoothDetails(
         return 0
     }
 
+    fun contentEquals(other: Any?): Boolean {
+        if (other !is BluetoothDetails) {
+            return false
+        }
+        return mac == other.mac
+    }
+
     companion object CREATOR : Parcelable.Creator<BluetoothDetails> {
+
         override fun createFromParcel(parcel: Parcel): BluetoothDetails {
             return BluetoothDetails(parcel)
         }
@@ -37,12 +45,19 @@ data class BluetoothDetails(
         override fun newArray(size: Int): Array<BluetoothDetails?> {
             return arrayOfNulls(size)
         }
-    }
 
-    fun contentEquals(other: Any?): Boolean {
-        if (other !is BluetoothDetails) {
-            return false
+        private fun generateUuid(mac: String, service: String): UUID {
+            val macBytes = mac.split(':')
+            assert(macBytes.size == 6)
+            var mostSigBits = 0L
+            var leastSigBits = 0L
+            macBytes.forEach {
+                mostSigBits = mostSigBits.shl(8) + it.toLong(16)
+            }
+            for(i in 0 until 8) {
+                leastSigBits = leastSigBits.shl(8) + service[i].toByte() //Cast to byte to force 16 bit unicode into 8 bits.
+            }
+            return UUID(mostSigBits, leastSigBits)
         }
-        return mac == other.mac
     }
 }
