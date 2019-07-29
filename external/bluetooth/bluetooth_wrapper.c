db6e29e6fd2865e05e319597b6cdbe649ea94c7e
Jan Phillip Kretzschmar <janphkre@gmx.de>
2019-07-29 20:36:14 +0200

    External: Try out dbus instead of bluetooth sdp

--- external/bluetooth/bluetooth_wrapper.c
+++ external/bluetooth/bluetooth_wrapper.c
@@ -4,8 +4,7 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 #include <bluetooth/rfcomm.h>
-#include <bluetooth/sdp.h>
-#include <bluetooth/sdp_lib.h>
+#include <dbus/dbus.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -20,54 +19,68 @@ void hookBluetoothSocket(int bluetoothSocket, int targetPort);
 int startLocalClientSocket(int targetPort);
 int pipeData(int sourceSocket, int targetSocket, char* buffer);
 int generateUuid(bdaddr_t bdaddr, const char* service_name, uint32_t* uuid);
-int defineService(int bluetoothPort, bdaddr_t bdaddr, const char* service_name, const char* service_dsc, const char* service_prov, sdp_session_t** session);
+int defineService(bdaddr_t bdaddr, const char* service_name, DBusConnection** conn);
+int setubDbus();
+int registerUuidDbus(const char* bluetoothDevice, const char* uuidString);
+int handleUuidDbusReply(DBusPendingCall* pending);
 
 int main(int argc, char **argv) {
     int serverPort, bluetoothPort, bluetoothDevice;
     char* service_name;
 
     if(argc != 5) {
-        printf("Usage:\nWrappes a server in a bluetooth connection.\nFirst argument is the server port,\n Second is the desired bluetooth port,\nThird is the bluetooth device id,\nFourth is the service name.");
-    }
-    if (sscanf (argv[1], "%i", &serverPort) != 1) {
-        printf("ERR: Server port is not an integer!");
+        printf("Usage:\nWrappes a server in a bluetooth connection.\nFirst argument is the server port,\nSecond is the desired bluetooth port,\nThird is the bluetooth device id,\nFourth is the service name.\n");
         return -1;
     }
-    if (sscanf (argv[2], "%i", &bluetoothPort) != 1) {
-        printf("ERR: Bluetooth port is not an integer!");
+    if (sscanf (argv[1], "%i", &serverPort) != 1) {
+        printf("ERR: Server port is not an integer!\n");
         return -2;
     }
-    if (sscanf (argv[3], "%i", &bluetoothDevice) != 1) {
-        printf("ERR: Bluetooth device id is not an integer!");
+    if (sscanf (argv[2], "%i", &bluetoothPort) != 1) {
+        printf("ERR: Bluetooth port is not an integer!\n");
         return -3;
+    }
+    if (sscanf (argv[3], "%i", &bluetoothDevice) != 1) {
+        printf("ERR: Bluetooth device id is not an integer!\n");
+        return -4;
     }
     service_name = argv[4];
     if(sizeof(service_name) < 8) {
-        printf("ERR: The service name should have at least 6 letters.");
+        printf("ERR: The service name should have at least 8 letters.\n");
         return -5;
     }
 
     bdaddr_t bdaddr = { 0 };
-    hci_devba(bluetoothDevice, &bdaddr);
-    int bluetoothSocket = startBluetoothServerSocket(bluetoothPort, bdaddr);
-
-    if (bluetoothSocket < 0) {
+    if(hci_devba(bluetoothDevice, &bdaddr) < 0) {
+        printf("ERR: No device found for the given device id %i.\n", bluetoothDevice);
         return -6;
     }
-
-    sdp_session_t* sdpSession;
-    if (defineService(bluetoothPort, bdaddr, service_name, "", "", &sdpSession)) {
-        printf("ERR: Failed to define the service!");
-        sdp_close(sdpSession);
-        close(bluetoothSocket);
+    int bluetoothSocket = startBluetoothServerSocket(bluetoothPort, bdaddr);
+    if (bluetoothSocket < 0) {
         return -7;
     }
+
+    const char* bluetoothDeviceString = strcat("hci", argv[3]);
+    DBusConnection* conn;
+    const char* uuidString = generateUuid(bdaddr, service_name, service_uuid_int);
+
+    if (defineService(bluetoothPort, bdaddr, bluetoothDeviceString, service_name, "", "", &conn, uuidString)) {
+        printf("ERR: Failed to define the service!\n");
+        if(NULL != conn) {
+            dbus_connection_close(conn);
+        }
+        close(bluetoothSocket);
+        return -8;
+    }
+return 0;
     while(1) {
         int client = acceptBluetoothSocket(bluetoothSocket);
         hookBluetoothSocket(client, serverPort);
     }
-    sdp_close(sdpSession);
+    unregisterUuidDbus(bluetoothDeviceString, uuidString);//TODO!
+    dbus_connection_close(conn);
     close(bluetoothSocket);
+    printf("\n");
     return 0;
 }
 
@@ -79,7 +92,7 @@ int startBluetoothServerSocket(int bluetoothPort, bdaddr_t bdaddr) {
     // allocate socket
     s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
     if (s == -1) {
-        printf("ERR: Could not create socket");
+        printf("ERR: Could not create bluetooth socket.\n");
         return -1;
     }
 
@@ -92,7 +105,7 @@ int startBluetoothServerSocket(int bluetoothPort, bdaddr_t bdaddr) {
 
     // put socket into listening mode
     if(listen(s, LISTEN_QUEUE_SIZE) < 0) {
-        printf("ERR: Failed to listen on socket.");
+        printf("ERR: Failed to listen on bluetooth socket.\n");
         close(s);
         return -2;
     }
@@ -131,7 +144,7 @@ int startLocalClientSocket(int targetPort) {
     //Create socket
     s = socket(AF_INET, SOCK_STREAM , 0);
     if (s == -1) {
-        printf("ERR: Could not create socket");
+        printf("ERR: Could not create local socket.\n");
         return -1;
     }
 
@@ -141,7 +154,7 @@ int startLocalClientSocket(int targetPort) {
     rem_addr.sin_port = htons(targetPort);
 
     if (connect(s, (struct sockaddr *)&rem_addr , sizeof(rem_addr)) < 0) {
-        printf("ERR: Connect failed.");
+        printf("ERR: Connect failed.\n");
         close(s);
         return -2;
     }
@@ -153,18 +166,18 @@ int pipeData(int sourceSocket, int targetSocket, char* buffer) {
 
     bytes_read = read(sourceSocket, buffer, BUF_SIZE);
     if(bytes_read < 0) {
-        printf("ERR: recv failed");
+        printf("ERR: recv failed.\n");
         return -1;
     }
 
     bytes_sent = send(targetSocket, buffer, bytes_read, 0);
     if(bytes_sent < 0) {
-        printf("ERR: send failed");
+        printf("ERR: send failed\n");
         return -2;
     }
 
     if(bytes_read != bytes_sent) {
-        printf("ERR: Read %d bytes but sent %d bytes", bytes_read, bytes_sent);
+        printf("ERR: Read %d bytes but sent %d bytes.\n", bytes_read, bytes_sent);
         //TODO: THIS MAY INDICATE A FAILURE
     }
     memset(buffer, 0, BUF_SIZE);
@@ -173,6 +186,10 @@ int pipeData(int sourceSocket, int targetSocket, char* buffer) {
 
 int generateUuid(bdaddr_t bdaddr, const char* service_name, uint32_t* uuid) {
     uint8_t* macAddress = (uint8_t*) &bdaddr;
+    char macAddressString[18];
+    ba2str(&bdaddr, macAddressString);
+    printf("Mac address is: %s\n", macAddressString);
+
     uuid[0] = macAddress[0];
     uuid[0] = (uuid[0] << 8) + macAddress[1];
     uuid[0] = (uuid[0] << 8) + macAddress[2];
@@ -190,63 +207,101 @@ int generateUuid(bdaddr_t bdaddr, const char* service_name, uint32_t* uuid) {
 }
 
 // Mostly taken from: https://people.csail.mit.edu/albert/bluez-intro/x604.html
-int defineService(int bluetoothPort, bdaddr_t bdaddr, const char* service_name, const char* service_dsc, const char* service_prov, sdp_session_t** session) {
-    uint32_t service_uuid_int[] = { 0, 0, 0, 0 };
-    uint8_t rfcomm_channel = (uint8_t) bluetoothPort;
-
-    uuid_t root_uuid, rfcomm_uuid, svc_uuid;
-    sdp_list_t *rfcomm_list = 0,
-               *root_list = 0,
-               *proto_list = 0, 
-               *access_proto_list = 0;
-    sdp_data_t *channel = 0;
-
-    sdp_record_t *record = sdp_record_alloc();
-
-    //generate UUID
-    if(generateUuid(bdaddr, service_name, service_uuid_int) < 0) {
+int defineService(bdaddr_t bdaddr, const char* service_name, DBusConnection** conn) {
+    uint32_t uuid[4] = { 0, 0, 0, 0 };
+    if(generateUuid(bdaddr, service_name, uuid) < 0) {
         return -1;
     }
-
-    // set the general service ID
-    sdp_uuid128_create( &svc_uuid, &service_uuid_int );
-    sdp_set_service_id( record, svc_uuid );
-
-    // make the service record publicly browsable
-    sdp_uuid16_create(&root_uuid, PUBLIC_BROWSE_GROUP);
-    root_list = sdp_list_append(0, &root_uuid);
-    sdp_set_browse_groups( record, root_list );
-
-    // set rfcomm information
-    sdp_uuid16_create(&rfcomm_uuid, (uint16_t) service_uuid_int[2]);//TODO: cast MAY BE INCORRECT
-    channel = sdp_data_alloc(SDP_UINT8, &rfcomm_channel);
-    rfcomm_list = sdp_list_append( 0, &rfcomm_uuid );
-    sdp_list_append( rfcomm_list, channel );
-    sdp_list_append( proto_list, rfcomm_list );
-
-    // attach protocol information to service record
-    access_proto_list = sdp_list_append( 0, proto_list );
-    sdp_set_access_protos( record, access_proto_list );
-
-    // set the name, provider, and description
-    sdp_set_info_attr(record, service_name, service_prov, service_dsc);
-
-
-    int err = 0;
-
-    // connect to the local SDP server, register the service record, and 
-    // disconnect
-    *session = sdp_connect(&bdaddr, BDADDR_LOCAL, SDP_RETRY_IF_BUSY);//TODO: bdaddr may have to revert back to BDADDR_ANY
-    err = sdp_record_register(*session, record, 0);
-
-    // cleanup
-    sdp_data_free(channel);
-    sdp_list_free(rfcomm_list, 0);
-    sdp_list_free(root_list, 0);
-    sdp_list_free(access_proto_list, 0);
-
-    if(err < 0) {
+    //TODO: CONVERT UUID INT ARRAY TO STRING!!!
+    if(setupDbus(conn) < 0) {
         return -2;
     }
+    
+    if(registerUuidDbus() < 0) {
+        dbus_connection_close(*conn);
+        return -3;
+    }
+
+    return 0;
+}
+
+//Taken from http://www.matthew.ath.cx/misc/dbus
+int setubDbus(DBusConnection** conn) {
+    DBusError err;
+    int ret;
+    // initialise the errors
+    dbus_error_init(&err);
+
+    // connect to the bus
+    *conn = dbus_bus_get(DBUS_BUS_SESSION, &err);
+    if (dbus_error_is_set(&err)) { 
+        printf("ERR: Failed DBus connection (%s)\n", err.message); 
+        dbus_error_free(&err); 
+    }
+    if (NULL == conn) { 
+        return -1;
+    }
+    return 0;
+}
+
+//Taken from http://www.matthew.ath.cx/misc/dbus
+//Documentation for bluez method can be found at https://git.kernel.org/pub/scm/bluetooth/bluez.git/tree/doc/network-api.txt
+int registerUuidDbus(const char* bluetoothDevice, const char* uuidString) {
+    DBusMessage* msg;
+    DBusMessageIter args, options;
+    DBusPendingCall* pending;
+    msg = dbus_message_new_method_call("org.bluez", // target for the method call
+        "/org/bluez", // object to call on
+        "org.bluez.ProfileManager1", // interface to call on
+        "RegisterProfile"); // method name
+    if (NULL == msg) { 
+        printf("ERR: DBus Message Null\n");
+        return -2;
+    }
+
+    // append arguments
+    dbus_message_iter_init_append(msg, &args);
+//CREATE ARBITRARY OBJECT?
+    if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_OBJECT, ADDRESS OF THIS PROFILE???)) { 
+        printf("ERR: DBus Out Of Memory!\n"); 
+        return -3;
+    }
+    if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &uuidString)) { 
+        printf("ERR: DBus Out Of Memory!\n"); 
+        return -4;
+    }
+//TODO: CREATE DICTIONARY
+//TODO: WRITE CHANNEL tO DICTIONARY
+    dbus_message_iter_open_container(&args, DBUS_TYPE_ARRAY, "{sssqqbbbsqq}", &options);
+    dbus_message_iter_close_container(&args, &options);    
+
+    if (!dbus_message_iter_append_container(&args, DBUS_TYPE_DICT, dictionary)) { 
+        printf("ERR: DBus Out Of Memory!\n"); 
+        return -4;
+    }
+
+    // send message and get a handle for a reply
+    if (!dbus_connection_send_with_reply (conn, msg, &pending, -1)) { // -1 is default timeout
+        dbus_message_unref(msg);
+        printf("ERR: DBus Out Of Memory!\n"); 
+        return -10;
+    }
+    if (NULL == pending) {
+        dbus_message_unref(msg);
+        printf("ERR: Pending Call Null\n"); 
+        return -11;
+    }
+    dbus_connection_flush(conn);
+
+    // free message
+    dbus_message_unref(msg);
+
+    return handleUuidDbusReply(pending);
+}
+
+//Taken from http://www.matthew.ath.cx/misc/dbus
+int handleUuidDbusReply(DBusPendingCall* pending) {
+    // block until we receive a reply
+    dbus_pending_call_block(pending);
     return 0;
 }
