0db07998c7be340c7d5025e0adff7234508126c1
Jan Phillip Kretzschmar <janphkre@gmx.de>
2019-08-04 16:55:57 +0200

    External: reverted back to server-socket, thus removing dbus

--- external/bluetooth/build.sh
+++ external/bluetooth/build.sh
@@ -7,6 +7,6 @@ pwd
 rm -rf ./build
 mkdir ./build
 cd ./build
-gcc -g -c -Wall -Werror -fpic ../bluetooth_wrapper.c $(pkg-config --cflags dbus-1) 
-gcc -g -o bluetooth_wrapper ./bluetooth_wrapper.o -lbluetooth -lpthread $(pkg-config --libs dbus-1) 
+gcc -g -c -Wall -Werror -fpic ../bluetooth_wrapper.c
+gcc -g -o bluetooth_wrapper ./bluetooth_wrapper.o -lbluetooth -lpthread
 
