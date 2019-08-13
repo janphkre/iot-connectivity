9782b3d9d4a2d11a15bc364a15384faccdda6eac
Jan Phillip Kretzschmar <janphkre@gmx.de>
2019-08-13 23:36:08 +0200

    Android: Tried implementation of htt wrapper with socket factory

---
+++ android-app/p2p/src/main/java/de/zweidenker/p2p/connection/bluetooth/UnusedBluetoothSocketFactory.kt
@@ -0,0 +1,57 @@
+package de.zweidenker.p2p.connection.bluetooth
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothSocket
+import de.zweidenker.p2p.P2PModule
+import de.zweidenker.p2p.model.Device
+import java.net.InetAddress
+import java.net.Socket
+import java.net.SocketImpl
+import javax.net.SocketFactory
+
+@Deprecated("Use the BluetoothConnectionStream instead")
+class UnusedBluetoothSocketFactory(
+    private val device: Device
+): SocketFactory() {
+
+    private class WrappingSocket(socketImpl: SocketImpl): Socket(socketImpl)
+
+    override fun createSocket(): Socket {
+        val bluetoothAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+            ?: throw UnsupportedOperationException("Device does not support Bluetooth")
+
+        if (!bluetoothAdapter.isEnabled) {
+            bluetoothAdapter.enable()
+            Thread.sleep(P2PModule.BLUETOOTH_ENABLE_SLEEP_MS)
+        }
+
+        val bluetoothDevice = bluetoothAdapter.bondedDevices?.firstOrNull { bluetoothDevice ->
+            bluetoothDevice.address == device.bluetoothDetails.mac
+        } ?: bluetoothAdapter.getRemoteDevice(device.bluetoothDetails.mac)
+
+        bluetoothAdapter.cancelDiscovery()
+
+        val method = bluetoothDevice::class.java.getMethod("createInsecureRfcommSocket", Int::class.javaPrimitiveType)
+        val socket = method.invoke(bluetoothDevice, device.bluetoothDetails.port) as BluetoothSocket
+
+        return WrappingSocket(UnusedWrappingBluetoothSocketImpl(socket))
+    }
+
+    override fun createSocket(host: String?, port: Int): Socket {
+        val socket = createSocket()
+        socket.connect(null)
+        return socket
+    }
+
+    override fun createSocket(host: String?, port: Int, localHost: InetAddress?, localPort: Int): Socket {
+        return createSocket(host, port)
+    }
+
+    override fun createSocket(host: InetAddress?, port: Int): Socket {
+        return createSocket(host, port)
+    }
+
+    override fun createSocket(address: InetAddress?, port: Int, localAddress: InetAddress?, localPort: Int): Socket {
+        return createSocket(address?.hostName, port)
+    }
+}
---
+++ android-app/p2p/src/main/java/de/zweidenker/p2p/connection/bluetooth/UnusedWrappingBluetoothSocketImpl.kt
@@ -0,0 +1,70 @@
+package de.zweidenker.p2p.connection.bluetooth
+
+import android.bluetooth.BluetoothSocket
+import java.io.InputStream
+import java.io.OutputStream
+import java.net.InetAddress
+import java.net.SocketAddress
+import java.net.SocketImpl
+
+class UnusedWrappingBluetoothSocketImpl(
+    private val bluetoothSocket: BluetoothSocket
+): SocketImpl() {
+
+    override fun listen(backlog: Int) {
+        throw UnsupportedOperationException("Listen is not supported on a BluetoothSocket!")
+    }
+
+    override fun getOption(optID: Int): Any? {
+        //throw UnsupportedOperationException("GetOption is not supported on a BluetoothSocket!")
+        return null
+    }
+
+    override fun create(stream: Boolean) { }
+
+    override fun setOption(optID: Int, value: Any?) {
+        //throw UnsupportedOperationException("SetOption is not supported on a BluetoothSocket!")
+    }
+
+    override fun connect(host: String?, port: Int) {
+        bluetoothSocket.connect()
+    }
+
+    override fun connect(address: InetAddress?, port: Int) {
+        bluetoothSocket.connect()
+    }
+
+    override fun connect(address: SocketAddress?, timeout: Int) {
+        bluetoothSocket.connect()
+    }
+
+    override fun bind(host: InetAddress?, port: Int) {
+        throw UnsupportedOperationException("Bind is not supported on a BluetoothSocket!")
+    }
+
+    override fun accept(s: SocketImpl?) {
+        throw UnsupportedOperationException("Accept is not supported on a BluetoothSocket!")
+    }
+
+    override fun getOutputStream(): OutputStream {
+        return bluetoothSocket.outputStream
+    }
+
+    override fun available(): Int {
+        return 0
+    }
+
+    override fun sendUrgentData(data: Int) {
+        throw UnsupportedOperationException("SendUrgentData is not supported on a BluetoothSocket!")
+    }
+
+    override fun getInputStream(): InputStream {
+        return bluetoothSocket.inputStream
+    }
+
+    override fun close() {
+        return bluetoothSocket.close()
+    }
+
+
+}
